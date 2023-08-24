863d6d1428e7a967f786a8140b98aef65f173092
Bruna Nobrega <121074742+nobregaabruna@users.noreply.github.com>
2023-08-24 15:20:03 -0300

    Update App.js
    Tirei o complemento pois apenas com o cep não existe a informação de complemento

--- App.js
+++ App.js
@@ -52,7 +52,6 @@ function App() {
           <h2>CEP: {cep.cep}</h2>
 
           <span>{cep.logradouro}</span>
-          <span>Complemento: {cep.complemento}</span>
           <span>{cep.bairro}</span>
           <span>{cep.localidade} - {cep.uf}</span>
 
